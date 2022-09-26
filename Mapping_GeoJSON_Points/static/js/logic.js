15f6b582f42716f5bb89abffbd44a26a0fe41f38
Daniel Mast <danielmastnor@gmail.com>
2022-09-26 12:45:01 -0400

    reading json from github

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,5 +1,6 @@
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
+
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -11,14 +12,12 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-ni
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/DanielMastCA/Mapping_Earthquakes/main/majorAirports.json";
+
 // Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport, {
-    // We turn each feature into a marker on the map.
-    onEachFeature: function (feature, layer) {
-        console.log(layer);
-        layer
-            .bindPopup("<h2>" + "Airport Code: " + feature.properties.faa + "</h2>"
-                + "<hr>"
-                + "<h3>" + "Airport Name: " + feature.properties.name + "</h3>");
-    }
-}).addTo(map);
+d3.json(airportData).then(function (data) {
+    console.log(data);
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJSON(data).addTo(map);
+});
