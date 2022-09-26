8de738754aaae7b9a5783afc35fba9bf297a2c2f
Daniel Mast <danielmastnor@gmail.com>
2022-09-26 12:40:14 -0400

    Mapping single point from local file

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,8 +1,8 @@
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -11,21 +11,14 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-//  Add a marker to the map for Los Angeles, California.
-
-
-let cityData = cities;
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function (city) {
-    L.marker(city.location)
-        .addTo(map);
-
-    L.circleMarker(city.location, {
-        radius: (city.population - 200000) / 100000,
-        color: 'yellow',
-        fillColor: 'yellow',
-        lineweight: 4
-    })
-        .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-        .addTo(map);
-});
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    onEachFeature: function (feature, layer) {
+        console.log(layer);
+        layer
+            .bindPopup("<h2>" + "Airport Code: " + feature.properties.faa + "</h2>"
+                + "<hr>"
+                + "<h3>" + "Airport Name: " + feature.properties.name + "</h3>");
+    }
+}).addTo(map);
